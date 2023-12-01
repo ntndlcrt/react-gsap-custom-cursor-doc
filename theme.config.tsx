c470deaca80b8908c8292de8ea56cae88aaef3ec
adelcourte <adelcourte.pro@gmail.com>
2023-12-01 03:12:59 +0100

    First commit

---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { DocsThemeConfig } from "nextra-theme-docs"
+
+const config: DocsThemeConfig = {
+  logo: <span>React GSAP Custom Cursor</span>,
+  project: {
+    link: "https://github.com/ntndlcrt/react-gsap-custom-cursor",
+  },
+  footer: {
+    text: "&copy; Antoine Delcourte - 2k23",
+  },
+}
+
+export default config
